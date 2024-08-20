e8bf1d785206ec239cb7f10e2b22c40258f484c3
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-08-20 12:20:42 +0200

    First commit
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

---
+++ app/src/main/java/io/seqera/debug/UnsafeTracerAgent.java
@@ -0,0 +1,44 @@
+package io.seqera.debug;
+
+/**
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+
+import java.lang.instrument.Instrumentation;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
+
+public class UnsafeTracerAgent {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.err.println("=== Starting UnsafeTracerAgent premain ===");
+        new AgentBuilder.Default()
+                .ignore(ElementMatchers.none())
+                .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+                .disableClassFormatChanges() 
+                .type(ElementMatchers.named("sun.misc.Unsafe"))
+                .transform((builder, typeDescription, classLoader, module, domain) -> {
+                    System.out.println("Transforming: " + typeDescription.getName());
+                    return builder
+                            .visit(Advice.to(MyAdvice.class)
+                                    .on(ElementMatchers.any()));
+                })
+                .installOn(inst);
+    }
+
+    public static class MyAdvice {
+
+        @Advice.OnMethodEnter
+        public static void onEnter(@Advice.Origin String method, @Advice.AllArguments Object[] args) {
+            System.err.println("Intercepted method: " + method + " with arguments: " + java.util.Arrays.toString(args));
+        }
+
+        @Advice.OnMethodExit
+        public static void onExit(@Advice.Origin String method, @Advice.Return Object returnValue) {
+            System.err.println("Exiting method: " + method + " with return value: " + returnValue);
+        }
+
+    }
+}
---
+++ app/src/test/groovy/io/seqera/debug/UnsafeTest.java
@@ -0,0 +1,22 @@
+package io.seqera.debug;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+public class UnsafeTest {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Page size: " + Unsafe.getUnsafe().pageSize());
+
+        Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+
+        long memoryAddress = unsafe.allocateMemory(1024);
+        System.out.println("Memory allocated at: " + memoryAddress);
+
+        unsafe.freeMemory(memoryAddress);
+        System.out.println("Memory freed.");
+    }
+}
