18b8b028f0d9de3663cad598d7d1ae611d30d252
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-08-21 10:30:11 +0200

    Refactor build + add publish script
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 java \
-  -javaagent:$PWD/app/build/libs/unsafe-tracker-0.1.0.jar \
-  -Xbootclasspath/a:$PWD/app/build/libs/unsafe-tracker-0.1.0.jar \
-  -cp ./app/build/classes/groovy/test \
+  -javaagent:$PWD/build/libs/unsafe-tracker-0.1.0.jar \
+  -Xbootclasspath/a:$PWD/build/libs/unsafe-tracker-0.1.0.jar \
+  -cp ./build/classes/groovy/test \
   io.seqera.debug.UnsafeTest
